b63bc916fb3a2c4ae4cf97e971373c3a5dddc667
clement guellec <clementguellec@gmail.com>
2023-08-11 15:50:06 +0200

    /add comment for rt an storage

--- src/pyrebase_file_storage.py
+++ src/pyrebase_file_storage.py
@@ -1,20 +1,51 @@
+"""
+all function to manipulate the file storage
+can store picture and text file
+"""
+
 import pyrebase
 import config.pyrebase_config as pyrebase_config
 
 
-def upload(storage, filename, online_filename, *path):
+def upload(storage, filename:str, online_filename:str, *path):
+    """
+    upload a file on firebase storage
+
+    Args:
+        storage (pyrebase.storage): pyrebase storage object type, enable to interact with the pyrebase storage
+        filename (str): name of the file you want to upload, position is file root, or the file path must be in the variable
+        online_filename (str): name of the file you want in the storage
+        *path(list[str]): path of the file in the storage
+    """
     current_position = storage
     for position in path:
         current_position = current_position.child(position)
     current_position.child(online_filename).put(filename)
 
-def download(storage, filename, online_filename, *path):
+def download(storage, filename:str, online_filename:str, *path):
+    """download a file from the firebase storage
+
+    Args:
+        storage (pyrebase.storage): pyrebase storage object type, enable to interact with the pyrebase storage
+        filename (str): name of the file you want to download, position is file root, or the file path must be in the variable
+        online_filename (str): name of the file in the storage
+        *path(list[str]): path of the file in the storage
+    """
     current_position = storage
     for position in path:
         current_position = current_position.child(position)
     current_position.child(online_filename).download(filename)
 
-def remove(storage, online_filename, *path):
+def remove(storage, online_filename:str, *path):
+    """
+    remove a file from firebase storage
+    not working
+
+    Args:
+        storage (pyrebase.storage): pyrebase storage object type, enable to interact with the pyrebase storage
+        online_filename (str): name of the file in the storage
+        *path(list[str]): path of the file in the storage
+    """
     current_position = storage
     for position in path:
         current_position = current_position.child(position)
--- src/pyrebase_realtimedb.py
+++ src/pyrebase_realtimedb.py
@@ -1,21 +1,45 @@
+"""
+all the function to work with the realtime database
+the realtime database is organised like JSON
+"""
+
 import pyrebase
 
 
 
 
-def set_data(firebase_database,data, *path):
+def set_data(firebase_database,data:dict, *path):
+    """
+    insert data in the realtime database
+    Args:
+        firebase_database (pyrebase.database): pyrebase database object type, enable to interact with the realtime database
+        data (dict): information you want to add
+    """
     current_position = firebase_database
     for position in path:
         current_position = current_position.child(position)
     current_position.set(data)
 
 def remove_data(firebase_database,*path):
+    """
+    remove a data in the realtime database
+    Args:
+        firebase_database (pyrebase.database): _description_
+    """
     current_position = firebase_database
     for position in path:
         current_position = current_position.child(position)
     current_position.remove()
 
 def get_data(firebase_database,*path):
+    """_summary_
+
+    Args:
+        firebase_database (pyrebase.database): _description_
+
+    Returns:
+        _type_: _description_
+    """
     current_position = firebase_database
     for position in path:
         current_position = current_position.child(position)
